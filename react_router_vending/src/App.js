c3bf98bb588fbaef1c597b82ba0b33184d72f324
Navin N. Singh <30394693+navinsinghmed@users.noreply.github.com>
2023-06-23 16:48:16 -0400

    Add files via upload

---
+++ react_router_vending/src/App.js
@@ -0,0 +1,30 @@
+import { BroswerRouter, Route } from 'react-router-dom';
+import VendingMachine from './VendingMachine';
+import Cookies from './Cookies';
+import SourPatchWatermelon from './SourPatchWatermelon';
+import CocaCola from './CocaCola';
+import NavBar from './NavBar';
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavBar />
+                <Route exact path="/cookies">
+                    <Cookies />
+                </Route>
+                <Route exact path="/sourpatchwatermelon">
+                    <SourPatchWatermelon />
+                </Route>
+                <Route exact path="/cocacola">
+                    <CocaCola />
+                </Route>
+                <Route exact path="/">
+                    <VendingMachine />
+                </Route>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
